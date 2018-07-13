d4e6fa6603c235b6d7a25468182bb2901528f4db
Kelly Cotter <kjc853@gmail.com>
2018-07-13 13:02:31 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -81,7 +81,9 @@ end
 end
 
 amounts(perfect_10_recipe)
+
 #6: Return the number of ingredients in the `perfect_10_recipe` hash.
+
 def number_ingredients(perfect_10_recipe)
   perfect_10_recipe.each do |ingredients|
     puts "There are #{ingredients.size} ingredients."
